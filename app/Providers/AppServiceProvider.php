41571f6336eb800560e6d9146741d7faa8c6aeec
josemagc7 <josemag.c.77@gmail.com>
2023-05-02 18:45:55 +0200

    papa

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,8 +12,9 @@ class AppServiceProvider extends ServiceProvider
      * Register any application services.
      */
     public function register(): void
-    {
+    {print_r("a");
         if (env('REDIRECT_HTTPS')) {
+            print_r("b");
             $this->app['request']->server->set('HTTPS', true);
         }
     }
@@ -23,8 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
+        print_r("1");
         if (env('REDIRECT_HTTPS')) {
             $url->formatScheme('https://');
+            print_r("2");    
         }
         Schema::defaultStringLength(191);
     }
