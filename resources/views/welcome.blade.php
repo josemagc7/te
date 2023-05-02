c9902cec409017abbb0ee8d684d3cf9d1b115951
josemagc7 <josemag.c.77@gmail.com>
2023-05-02 19:29:39 +0200

    papa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,6 +22,9 @@
                     @auth
                         <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
                     @else
+                    <?php
+                        print_r("aaa");
+                        ?>
                         <a href="{{ route('/login') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
                         @if (Route::has('register'))
