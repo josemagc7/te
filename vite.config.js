dc9a1456916d6bcac45141cc26929923a5804cb9
josemagc7 <josemag.c.77@gmail.com>
2023-05-03 18:20:54 +0200

    joko

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,68 @@
-import { defineConfig } from 'vite';
-import laravel, { refreshPaths } from 'laravel-vite-plugin';
+// import { defineConfig } from 'vite';
+// import laravel, { refreshPaths } from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: [
-                ...refreshPaths,
-                'app/Http/Livewire/**',
-            ],
-        }),
-    ],
-});
+// export default defineConfig({
+//     root: './app',
+//     build:{
+//         outDir: '../public/build',
+//         emptyOutDir: true,
+//         manifest:true,
+//         rollupOptions: {
+//             input: './resources/views/welcome.blade.php',
+//           },
+//         plugins: [
+//             laravel({
+//                 input: [
+//                     'resources/css/app.css',
+//                     'resources/js/app.js',
+//                 ],
+//                 refresh: [
+//                     ...refreshPaths,
+//                     'app/Http/Livewire/**',
+//                 ],
+//             }),
+//         ],
+//     },
+//     publicDir: '../public',
+// });
+
+import mix from 'laravel-mix';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+
+mix.js('resources/js/app.js', 'public/js')
+    .vue()
+    .alias({
+        '@': resolve('resources/js'),
+    })
+    .webpackConfig({
+        output: {
+            publicPath: '/',
+            chunkFilename: mix.inProduction() ? 'js/[name].[chunkhash].js' : 'js/[name].js',
+        },
+        resolve: {
+            extensions: ['.js', '.vue', '.json'],
+            alias: {
+                vue$: 'vue/dist/vue.runtime.esm.js',
+            },
+        },
+    });
+
+export default {
+    plugins: [vue()],
+    server: {
+        proxy: {
+            '/': {
+                target: 'http://localhost:8000',
+                changeOrigin: true,
+            },
+        },
+    },
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: true,
+    },
+    optimizeDeps: {
+        include: ['@inertiajs/inertia', '@inertiajs/inertia-vue3'],
+    },
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,18 @@
+// const mix = require('laravel-mix');
+
+// mix.js('resources/js/app.js', 'public/js')
+//    .sass('resources/sass/app.scss', 'public/css')
+//    .version();
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
-   .version();
+    .vue()
+    .postCss('resources/css/app.css', 'public/css', [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ]);
+
+if (mix.inProduction()) {
+    mix.version();
+}
