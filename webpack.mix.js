cb1d2d96919ebf2ecf2828dd91e85c7163d6ecbb
josemagc7 <josemag.c.77@gmail.com>
2023-05-16 16:51:27 +0200

    first commit

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,7 @@
 // mix.js('resources/js/app.js', 'public/js')
 //    .sass('resources/sass/app.scss', 'public/css')
 //    .version();
+mix.setPublicPath('public/build');
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
